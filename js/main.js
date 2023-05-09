a1e256dd166866eaee73c67d063ca5f81847274f
kezoneitor <keslerth.c@gmail.com>
2023-05-08 22:23:11 -0600

    Estilos y hacer botones dinamicos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-function GetUrl(code) {
-    const url = "https://docs.google.com/spreadsheets/d/18xu5gGVZR3xL5qxss1b1dhz7CDwN6T-bnqr6GTyax3c/export?format=csv"; // Reemplaza 'ID_DEL_DOCUMENTO' con el ID real de tu archivo de Google Sheets
+const escuelaDominicalUrl = "https://docs.google.com/spreadsheets/d/18xu5gGVZR3xL5qxss1b1dhz7CDwN6T-bnqr6GTyax3c/export?format=csv"; // Reemplaza 'ID_DEL_DOCUMENTO' con el ID real de tu archivo de Google Sheets
+const Enum = {
+    Text: 0,
+    Key: 1,
+    URL: 2,
+    Disabled: 3,
+    Hidden: 4
+}
 
-    fetch(url)
+function GetUrl(code) {
+    fetch(escuelaDominicalUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Error al obtener los datos: ${response.statusText}`);
@@ -10,21 +17,25 @@ function GetUrl(code) {
         })
         .then(data => {
             const lines = data.split('\n');
-            result = '';
-            
-            for (let i = 0; i < lines.length; i++) {
+            let isEnabled = false;
+            let result = '';
+
+            for (let i = 1; i < lines.length; i++) {
                 const currentLine = lines[i].trim();
                 if (!currentLine) continue;
 
                 const values = currentLine.split(',');
-                if (values[0] == code){
-                    result = values[1];
+                if (values[Enum.Key] == code) {
+                    result = values[Enum.URL];
+                    isEnabled = values[Enum.Disabled] == "";
                     break;
                 }
             }
 
-            if(result)
-                window.open(result, '_blank');
+            if (result) {
+                if(isEnabled)
+                    window.open(result, '_blank');
+            }
             else
                 console.error("Error al obtener el url.");
         })
@@ -33,3 +44,47 @@ function GetUrl(code) {
         });
 }
 
+$(document).ready(function () {
+    fetch(escuelaDominicalUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error al obtener los datos: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const lines = data.split('\n');
+
+            for (let i = 1; i < lines.length; i++) {
+                const currentLine = lines[i].trim();
+                if (!currentLine) continue;
+
+                const values = currentLine.split(',');
+
+                // Crear un botón y asignarle un valor
+                const boton = $('<button></button>').text(values[Enum.Text]).val(values[Enum.Key]);
+
+                // Esta deshabilitado
+                const isEnabled = values[Enum.Disabled] == "";
+                boton.prop('disabled', !isEnabled)
+
+                // Esta oculto
+                const isHidden = values[Enum.Hidden] != "";
+                boton.prop('hidden', isHidden)
+
+                //Agregar clases
+                boton.addClass(`btn ${isEnabled ? '' : 'disabled'}`);
+
+                // Añadir un evento click al botón
+                boton.on('click', function () {
+                    GetUrl($(this).val());
+                });
+
+                // Añadir el botón clonado al contenedor
+                $('#btnList').append(boton);
+            }
+        })
+        .catch(error => {
+            console.error("Error al hacer fetch:", error);
+        });
+});
